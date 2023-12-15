523bc1c740d1c0277c7d08872cd989c6e2c3ed21
Atindra Singh Rathore <118996145+AtindraRathore@users.noreply.github.com>
2023-12-15 17:08:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const byteSize = (str) => {
   // write your code here
-	return str.length;
+	let a = new Blob([str]);
+	 return a.size;
 };
 
 // Do not change the code below
-const str = prompt("Enter some string.");
-alert(byteSize(str));
+// const str = prompt("Enter some string.");
+// alert(byteSize(str));
